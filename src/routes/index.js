7302fff6b888649bc46ef07883e79fbd19432290
Winterfuchs <jan.multimail@gmail.com>
2022-05-01 18:08:37 +0200

    Added routes and logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,43 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
+const app = express()
+const fs = require('fs');
 
-const todos = ["Do home work", "Relax"]
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const todos = {1: "Do home work", 2: "Relax"}
+
+// I know this will overwrite the file everytime you start the server but I think
+// it's fine for this example
+let data = JSON.stringify(todos);
+fs.writeFileSync('todos.json', data);
+
+let rawdata = fs.readFileSync('todos.json');
+let todo = JSON.parse(rawdata);
 
 // Home
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Todo App' })
+  let rawdataRead = fs.readFileSync('todos.json');
+  let todos = JSON.parse(rawdataRead);
+  res.render('index', { title: 'Todo App', todos: todos })
 })
+
+// Todos
+router.post('/todo', (req, res, next) => {
+  // check for the current index in the json file and add one
+  var index = Object.keys(todo).length+1;
+  // add the new todo to the json file
+  todo[index] = req.body["task"];
+  // write the new todo to the json file
+  data = JSON.stringify(todo);
+  fs.writeFileSync('todos.json', data);
+
+  console.log(todo)
+  res.redirect("/")
+})
+
+app.use("/", router);
 
 module.exports = router
